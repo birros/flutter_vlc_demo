c1e32f9b641c0693eb495f78e2b8fcad25989179
birros <birros@protonmail.com>
2021-08-21 16:22:30 +0200

    feat(macos): add communication between flutter & swift

--- macos/Runner/VideoController.swift
+++ macos/Runner/VideoController.swift
@@ -1,33 +1,88 @@
 import AppKit
 import VLCKit
 
+protocol VideoDelegate {
+  func videoDone()
+}
+
 class VideoController: NSViewController {
-    private var player: VLCMediaPlayer!
+  var delegate: VideoDelegate?
+  private var player: VLCMediaPlayer!
+  private var videoView: NSView!
 
-    override func loadView() {
-        self.view = NSView()
-        let url = NSURL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
-        let media = VLCMedia(url: url! as URL)
-        let player = VLCMediaPlayer()
-        self.player = player
-        self.player.media = media
-        self.player.drawable = self.view
-        self.player.play()
+  init(with uri: String) {
+    super.init(nibName: nil, bundle: nil)
+
+    let url = NSURL(string: uri)
+    let media = VLCMedia(url: url! as URL)
+
+    self.player = VLCMediaPlayer()
+    self.player.media = media
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func loadView() {
+    self.view = NSView()
+
+    self.videoView = NSView()
+    self.videoView.wantsLayer = true
+    self.videoView.layer?.backgroundColor = NSColor.black.cgColor
+    self.player.drawable = self.videoView
+    self.view.addSubview(self.videoView)
+
+    let button = NSButton(frame: NSMakeRect(0, 0, 100, 50))
+    button.title = "Back"
+    button.bezelStyle = .texturedSquare
+    button.isBordered = false
+    button.wantsLayer = true
+    button.layer?.backgroundColor = NSColor(
+      red: 0.26,
+      green: 0.26,
+      blue: 0.26,
+      alpha: 1.0
+    ).cgColor
+    button.target = self
+    button.action = #selector(VideoController.printSomething)
+    self.view.addSubview(button)
+  }
+
+  override func viewDidAppear() {
+    super.viewDidAppear()
+
+    self.videoView.frame = self.view.frame
+    self.fixVideoSize()
+
+    self.player.play()
+  }
+
+  override func viewDidLayout() {
+    super.viewDidLayout()
+
+    self.videoView.frame = self.view.frame
+  }
+
+  override func viewDidDisappear() {
+    super.viewDidDisappear()
+
+    self.player.stop()
+  }
+
+  func fixVideoSize() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      let frame = self.view.frame
+      self.videoView.frame = NSMakeRect(0, 0, frame.width + 1, frame.height + 1)
+
+      DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+        self.videoView.frame = frame
+      }
     }
+  }
 
-    override func viewDidAppear() {
-        super.viewDidAppear()
-        self.fixVideoSize()
-    }
-
-    func fixVideoSize() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            let frame = self.view.frame
-            self.view.frame = NSMakeRect(0, 0, frame.width + 1, frame.height + 1)
-
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                self.view.frame = frame
-            }
-        }
-    }
+  @objc
+  func printSomething() {
+    self.delegate?.videoDone()
+  }
 }
--- macos/Runner/WrapperController.swift
+++ macos/Runner/WrapperController.swift
@@ -1,43 +1,72 @@
 import AppKit
+import FlutterMacOS
 
-class WrapperController: NSViewController, NSWindowDelegate {
-    var flutterViewController: NSViewController?
-    var videoController: VideoController?
+class WrapperController: NSViewController, NSWindowDelegate, VideoDelegate {
+  var flutterViewController: FlutterViewController?
+  var videoController: VideoController?
 
-    init(with flutterViewController: NSViewController) {
-        super.init(nibName: nil, bundle: nil)
-        self.flutterViewController = flutterViewController
-        self.videoController = VideoController()
-    }
+  init(with flutterViewController: FlutterViewController) {
+    super.init(nibName: nil, bundle: nil)
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    self.flutterViewController = flutterViewController
 
-    override func loadView() {
-        self.view = NSView()
-        self.view.addSubview(self.flutterViewController!.view)
-        self.view.addSubview(self.videoController!.view)
-        // self.videoController?.view.removeFromSuperview()
-        // self.videoController = nil
-    }
+    let channel = FlutterMethodChannel.init(
+      name: "com.github.birros.flutter_vlc_demo/video",
+      binaryMessenger: flutterViewController.engine.binaryMessenger
+    )
+    channel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      if ("play" == call.method) {
+        let args = call.arguments as? Dictionary<String, Any>
+        let uri = args?["uri"] as? String
 
-    override func viewDidAppear() {
-        super.viewDidAppear()
-        self.view.window?.delegate = self
-        self.updateSize()
-    }
+        if (uri != nil && self.videoController == nil) {
+          self.videoController = VideoController.init(with: uri!)
+          self.videoController?.delegate = self
+          self.videoController?.view.frame = self.view.bounds
+          self.view.addSubview(self.videoController!.view)
+          self.flutterViewController?.view.isHidden = true;
+        }
 
-    func windowDidResize(_ notification: Notification) {
-        self.updateSize()
-    }
+        result(nil)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+  }
 
-    func updateSize() {
-        let width = self.view.window?.contentView?.bounds.width ?? 0
-        let height = self.view.window?.contentView?.bounds.height ?? 0
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 
-        self.view.setFrameSize(NSSize(width: width, height: height))
-        self.flutterViewController?.view.frame = self.view.bounds
-        self.videoController?.view.frame = self.view.bounds
-    }
+  override func loadView() {
+    self.view = NSView()
+    self.view.addSubview(self.flutterViewController!.view)
+  }
+
+  override func viewDidAppear() {
+    super.viewDidAppear()
+
+    self.view.window?.delegate = self
+    self.updateSize()
+  }
+
+  func windowDidResize(_ notification: Notification) {
+    self.updateSize()
+  }
+
+  func updateSize() {
+    let width = self.view.window?.contentView?.bounds.width ?? 0
+    let height = self.view.window?.contentView?.bounds.height ?? 0
+
+    self.view.setFrameSize(NSSize(width: width, height: height))
+    self.flutterViewController?.view.frame = self.view.bounds
+    self.videoController?.view.frame = self.view.bounds
+  }
+
+  func videoDone() {
+    self.flutterViewController?.view.isHidden = false;
+    self.videoController?.view.removeFromSuperview()
+    self.videoController = nil
+  }
 }
